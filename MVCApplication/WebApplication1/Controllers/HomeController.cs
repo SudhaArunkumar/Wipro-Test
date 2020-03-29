016ca77acd131e0187a87f512ef1a9eb73f02934
Sudha Arunkumar <sstsudha@gmail.com>
2020-03-29 22:40:35 +1100

    Final Commit

---
+++ MVCApplication/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebApplication1.Models;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            HomeViewModel model = new HomeViewModel();
+            return View(model);
+        }
+        [HttpPost]
+        public ActionResult Index(HomeViewModel model,string submit)
+        {
+            if(model != null)
+            {
+                model.OutputText = string.Format("Your Enter Text is {0}", model.Text);
+            }
+            return View(model);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ MVCApplication/WebApplication1/Models/HomeViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class HomeViewModel
+    {
+        public string Text { get; set; }
+        public string OutputText { get; set; }
+    }
+}
